d25f7434525f27a865c2ab96e23e0f51d6dad80b
Octopus <support@preprod.io>
2022-03-20 23:49:01 +0100

    [ADD] Product / Category Unit TEST

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\Category as CategoryController;
+use App\Models\Category;
+use App\Repositories\CategoryRepository;
+use App\Services\CategoryService;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_category_controller()
+    {
+        $categoryRepository = new CategoryRepository(new Category());
+        $categoryService = new CategoryService($categoryRepository);
+        $categoryController = new CategoryController($categoryService);
+
+        /**
+         * Test category list
+         */
+        $request = Request::create('/api/categories', 'GET');
+        $response = $categoryController->index($request);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test category create
+         */
+        $request = Request::create('/api/category/add', 'POST',[
+            'name'     =>  'AYOUB',
+        ]);
+        $response = $categoryController->store($request);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $id = json_decode($response->getContent(),1)["data"]["id"];
+
+        /**
+         * Test category show
+         */
+        $request = Request::create('/api/category/show/', 'GET');
+        $response = $categoryController->show($id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test category update
+         */
+        $request = Request::create('/api/category/update/', 'POST',[
+            'name'     =>  'AYOUB_EDITED',
+        ]);
+        $response = $categoryController->update($request, $id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test category delete
+         */
+        $request = Request::create('/api/category/delete/', 'DELETE');
+        $response = $categoryController->destroy($id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+}
---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\Product as ProductController;
+use App\Models\Product;
+use App\Repositories\ProductRepository;
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_product_controller()
+    {
+        $productRepository = new ProductRepository(new Product());
+        $productService = new ProductService($productRepository);
+        $productController = new ProductController($productService);
+
+        /**
+         * Test product list
+         */
+        $request = Request::create('/api/products', 'GET');
+        $response = $productController->index($request);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test product create
+         */
+        $request = Request::create('/api/product/add', 'POST',[
+            'name'     =>  'AYOUB',
+        ]);
+        $response = $productController->store($request);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $id = json_decode($response->getContent(),1)["data"]["id"];
+
+        /**
+         * Test product show
+         */
+        $request = Request::create('/api/product/show/', 'GET');
+        $response = $productController->show($id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test product update
+         */
+        $request = Request::create('/api/product/update/', 'POST',[
+            'name'     =>  'AYOUB_EDITED',
+        ]);
+        $response = $productController->update($request, $id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        /**
+         * Test product delete
+         */
+        $request = Request::create('/api/product/delete/', 'DELETE');
+        $response = $productController->destroy($id);
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+}
